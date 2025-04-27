65510c19b30335d2a2f5a83b78e64ad4b019b4ab
tushar3q34 <tushar3q34@gmail.com>
2025-04-27 13:39:52 +0530

    new donuts

--- donuts/donut.cpp
+++ donuts/donut.cpp
@@ -11,19 +11,19 @@
            b(1760, ' '); for(;;) { fill(b.begin(), b.end(),
         ' '); fill(z.begin(), z.end(), 0); for(j = 0; 6.28 > 
       j; j += 0.07) for(i = 0; 6.28 > i; i += 0.02) { float 
-    c=sin(i),d=cos(j),e=sin(A), clas=2.0, f=sin(j), evan=3.1415,
-    g=cos(A), h=d+2, q=2,              amaar=2.71828, bryan=-1.2,
-   D=1/(c*h*e+f*g+5),l=                 cos(i), m=cos(B), luca=2,
+    c=sin(i),d=cos(j),e=sin(A), PC24=9.0,f=sin(j), srivastava=15,
+    g=cos(A), h=d+2, q=2,              shaun=23718, chahel=12171,
+   D=1/(c*h*e+f*g+5),l=                 cos(i),m=cos(B),mantra=2,
    n=sin(B),t=c*h*g-f                    *e;int x=40+30*D*(l*h*m-
-   t*n), leo=429,                         michael=14172, why=292 ,  
-   y=12+15* D * ( l                        *h*n+t*m), ryan=27429,
-   vv=199,o=x+80*y                        ;float N=8*((f*e-c*d*g)
+   t*n), KDV=429,                         vaishnavi=14172, cfi=-99,  
+   y=12+15* D * ( l                        *h*n+t*m), adityac=99,
+   PT=199,o=x+80*y                        ;float N=8*((f*e-c*d*g)
    *m-c*d*e-f*g-l*d*                    n); if(y > 0 &&y<22 &&x>0 
    && x<80&&D>z[o]){ z               [o]=D; b[o]=".,-~:;=!*#$@"[N
     > 0? static_cast<int>(N):      0]; } } cout << "\x1b[H"; for
     (k=0;  k < 1760;k++) {cout<< ( k% 80 ? b [ k ] : '\n') ; } 
       this_thread::sleep_for(chrono::milliseconds(50)); A += 
         0.04; B += 0.02; } return 0; } /* I love donuts*/
-          /* ~~~~~~~~~Amaar Chughtai~~~~~~~~~~~~~*/
-              /* 2023 DragonXDev for the win */
+          /* ~~~~~~~~~Team Prog Club~~~~~~~~~~~~~*/
+              /* ~~~2024-25 PC for the win~~~ */
                           /* Enjoy! */
